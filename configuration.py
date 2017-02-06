de77df905aea8b16b2ed2351877b31777dc6a21b
Lee Campbell <lee@salk.edu>
2017-02-05 21:10:51 -0800

    redirecting to terrence.gaines

--- configuration.py
+++ configuration.py
@@ -4,6 +4,6 @@ default_filename = u"Access.xlsx"
 smartsheet_manager = u"Terrence Gaines <terrence.gaines@hp.com>"
 log_format = ".isoformat()"
 date_format = u"%Y-%m-%d %H:%M"
-redirect_emails_to = u"lee@snl.salk.edu"  #Normally an empty string.  Set to your email address to send all output to you. 
+redirect_emails_to = u"terrence.gaines@hp.com"  #Normally an empty string.  Set to your email address to send all output to you. 
 stop_after = 0  #Set this to zero to send all emails. 
 format='WVpFLWFkUi02SlEtcm80'
