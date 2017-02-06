f3f16519981096788acdc5f61414757b061be182
Lee Campbell <lee@salk.edu>
2017-02-05 22:41:03 -0800

    adding logo

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-c:\Python27\Scripts\pyinstaller.exe -F --data-data fonts --add-data hp.png paur.py 
+c:\Python27\Scripts\pyinstaller.exe -F --add-data fonts --add-data hp.png paur.py 
 move dist\paur.exe
 del /s/q dist
 del /s/q build
