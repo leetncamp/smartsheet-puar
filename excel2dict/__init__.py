9f4c9ace444b9d687a59f3b11fe85b7641404780
Lee Campbell <lee@salk.edu>
2017-02-05 17:54:15 -0800

    first build importing spreadsheet correctly now

---
+++ excel2dict/__init__.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""Convert a spreadsheet into a list of python dictionaries with keys pulled from row 1 as a header.
+Converts the first sheet (sheet 0), unless you pass in a sheet number."""
+
+import os
+import sys
+from pdb import set_trace as debug
+import openpyxl
+
+
+def excel2dict(path2xlsx, sheet=0):
+
+    """supply path2xlsx and get back the first sheet as a list of dictionaries with keys from the (required) header row.
+    Converts the first sheet unless you specify sheets=sheetnum"""
+
+
+    wb = openpyxl.load_workbook(path2xlsx)
+    ws =  wb.worksheets[sheet]
+    headers = {}
+
+    for cell in ws[1]:
+        headers[cell.col_idx - 1] = cell.value
+
+    data = []
+    for row in ws.iter_rows(row_offset=1):
+        rowdict = {}
+        for colnumber, header in headers.iteritems():
+            try:
+                rowdict[header] = row[colnumber].value
+            except Exception as e:
+                print traceback.format_exc()
+                raw_input("Press Enter to continue")
+        data.append(rowdict)
+    return(data) 
+
+
+if __name__=="__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("path", nargs="?", default="", help="path to excel file in .xlsx format")
+    parser.add_argument("--sheet", "-s", default=0, help="sheet to convert")
+
+
+    ns = parser.parse_args()
+
+    result = excel2dict(ns.path, sheet=ns.sheet)
+    print result
--- paur.py
+++ paur.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from openpyxl import load_workbook
+from excel2dict import excel2dict
 
 
 from argparse import ArgumentParser
@@ -8,7 +8,5 @@ parser = ArgumentParser(description=u"Notify non-HP smartsheet owners")
 parser.add_argument("filename", nargs="?", default="Access.xlsx", help=u"File to read.  Must be XLSX format.")
 ns = parser.parse_args()
 
-wb = load_workbook(ns.filename)
-
-
-
+data = excel2dict(ns.filename)
+print data
