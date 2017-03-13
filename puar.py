f5f14872994c253fabe7eca725896f194829fdea
Lee Campbell <lee@salk.edu>
2017-03-12 21:46:30 -0700

    execfile

--- puar.py
+++ puar.py
@@ -12,7 +12,11 @@ import os, sys
 UTC = pytz.timezone("UTC")
 LA = pytz.timezone("America/Los_Angeles")
 from snlmailer import Message
-from configuration import *  #pulls in variables defined in configuration.py
+#from configuration import *  #pulls in variables defined in configuration.py
+print os.getcwd()
+execfile("configuration.py")
+
+
 from collections import OrderedDict
 import datetime
 import base64
