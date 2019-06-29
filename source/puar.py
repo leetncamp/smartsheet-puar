1daf440857a723f5b01c17a42172bc243298ec5f
Lee Campbell <lee@salk.edu>
2019-06-28 18:38:58 -0700

    switched to a simpler to use progressbar

--- source/puar.py
+++ source/puar.py
@@ -17,26 +17,13 @@ import pytz
 UTC = pytz.timezone("UTC")
 LA = pytz.timezone("America/Los_Angeles")
 from snlmailer import Message
-import shutil
-
-
-
-Subject            = "Smartsheet non-HP collaborator report"
-From               = "smartsheet.hpadmin@hp.com"
-#smtp_server       = "smtp.snl.salk.edu"
-default_filename   = "UserAccessReport.xlsx"
-stop_after         = 0  #Set this to zero to send all emails. 
-
-try:
-	from configuration import *  #pulls in variables defined in configuration.py and will overwrite the variables above. 
-except:
-	pass
+from configuration import *  #pulls in variables defined in configuration.py
 
 from collections import OrderedDict
 import datetime
 import base64
 from nameparser import HumanName
-import progressbar
+from tqdm import tqdm
 import openpyxl
 
 stop_after       = locals().get("stop_after", 0) #stop_after defaults to 0 if not present in configuration.py
@@ -53,7 +40,8 @@ parser.add_argument("filename", nargs="?", default=default_filename, help=u"File
 parser.add_argument("--go", action="store_true", help="with --go, the program does not wait for confirmation. It sends email. Useful for automated running. ")
 ns = parser.parse_args()
 
-path2xlsx = os.path.join(BASE_DIR, "reports", ns.filename)
+path2xlsx = os.path.join(BASE_DIR, "smartsheet-puar", ns.filename)
+
 mtime = datetime.datetime.fromtimestamp(os.path.getmtime(path2xlsx))
 
 
@@ -73,43 +61,41 @@ owners={}
 
 print("Reading spreadsheet written on {0}...\n".format(mtime.strftime("%a %b %d, %Y  %I:%M %p")))
 
-with progressbar.ProgressBar(widgets=[progressbar.Percentage(), progressbar.Bar()], max_value=ws.max_row) as bar:
-    for xlrow in ws.iter_rows(row_offset=1):
-    	count += 1
-        row = {}
-        for colnumber, header in headers.iteritems():
-            row[header] = xlrow[colnumber].value
 
-        shared_to = row[u"Shared To"]
+for xlrow in tqdm(ws.iter_rows(row_offset=1)):
+    count += 1
+    row = {}
+    for colnumber, header in headers.iteritems():
+        row[header] = xlrow[colnumber].value
 
-        if shared_to and not hpRE.search(shared_to):
-        	owner = row[u"Owner"]
-        	owners[owner] = owners.get(owner, []) + [row]
-        	
-        else:
-        	continue #Skip this row if the Shared-to is an hp address
+    shared_to = row[u"Shared To"]
 
-        bar.update(xlrow[0].row)
+    if shared_to and not hpRE.search(shared_to):
+        owner = row[u"Owner"]
+        owners[owner] = owners.get(owner, []) + [row]
+        
+    else:
+        continue #Skip this row if the Shared-to is an hp address
+
+
 
 
 if stop_after in locals() and stop_after:
-	print(u"\nStopping after {0} emails".format(stop_after))
+    print(u"\nStopping after {0} emails".format(stop_after))
 
 if not ns.go:
-	redirect_emails_to = raw_input(u"\nSending {0} emails. Close the window to cancel. Press Enter to send. Type 'test'\nto redirect all emails to smartsheet.hpadmin@hp.com: ".format(len(owners)))
+    redirect_emails_to = raw_input(u"\nSending {0} emails. Close the window to cancel. Press Enter to send. Type 'test'\nto redirect all emails to smartsheet.hpadmin@hp.com: ".format(len(owners)))
 
-#Sometimes we don't want to rename the report file. 
-rename_report = True if redirect_emails_to else False
 
 if not (redirect_emails_to == '' or redirect_emails_to.replace("'","").lower() == 'test' or u"@" in redirect_emails_to):
-	print("Exiting. You must either press Return, type 'test', or supply an email address")
-	if not ns.go:
-		raw_input("Press any key to exit.")
-	os.system.exit()
+    print("Exiting. You must either press Return, type 'test', or supply an email address")
+    if not ns.go:
+        raw_input("Press any key to exit.")
+    os.system.exit()
 
 
 if redirect_emails_to == "test":
-	redirect_emails_to = "smartsheet.hpadmin@hp.com"
+    redirect_emails_to = "smartsheet.hpadmin@hp.com"
 
 
 
@@ -121,50 +107,44 @@ emails_sent = 0
 email_log = []
 
 print("Sending emails...")
-
 pb = progressbar.ProgressBar(widgets=[progressbar.Percentage(), progressbar.Bar()], max_value=len(owners)).start()
 
 for owner, rows in owners.iteritems():
 
-	for row in rows:
-		#normalize timezone to Pacific Time rather than UTC
-		row["Last Modified (PT)"] = LA.normalize(UTC.localize(row["Last Modified Date/Time (UTC)"])).strftime(date_format)
-		#row["owner_email"] = owner.replace(u"@hp.com", u"")
-		row["Sheet Name"] = row[u"Name"]
+    for row in rows:
+        #normalize timezone to Pacific Time rather than UTC
+        row["Last Modified (PT)"] = LA.normalize(UTC.localize(row["Last Modified Date/Time (UTC)"])).strftime(date_format)
+        #row["owner_email"] = owner.replace(u"@hp.com", u"")
+        row["Sheet Name"] = row[u"Name"]
 
 
-	nameStr = u" ".join([item.capitalize() for item in owner.split(u"@")[0].split(".")])
-	name = HumanName(nameStr)
-	firstname = name.first
-	lastname = name.last
-	html = template.render(**locals())
-	To = redirect_emails_to if redirect_emails_to else owner
-	msg = Message(To=To, From=From)
-	msg.Subject = Subject
-	msg.Html = html
-	addlHeaders = [["Precedence","bulk"], ['Disposition-Nofication-To', From]]
+    nameStr = u" ".join([item.capitalize() for item in owner.split(u"@")[0].split(".")])
+    name = HumanName(nameStr)
+    firstname = name.first
+    lastname = name.last
+    html = template.render(**locals())
+    To = redirect_emails_to if redirect_emails_to else owner
+    msg = Message(To=To, From=From)
+    msg.Subject = Subject
+    msg.Html = html
+    addlHeaders = [["Precedence","bulk"], ['Disposition-Nofication-To', From]]
 
-	errors = msg.customSend(smtp_server)
-
-	if errors:
-		print(errors)
-	emails_sent += 1
-	dt = eval(now)
-	log.write(u"{0}\tEmailed {1}\n".format(dt, msg.To))
-	email_log.append(owner)
-	pb.update(emails_sent)
-	if stop_after and emails_sent >= stop_after:
-		print(u"Stopping early at {0} emails. See configuration.py stop_after".format(stop_after))
-		break
-	
+    errors = msg.customSend(smtp_server)
+    if errors:
+        print(errors)
+    emails_sent += 1
+    dt = eval(now)
+    log.write(u"{0}\tEmailed {1}\n".format(dt, msg.To))
+    email_log.append(owner)
+    pb.update(emails_sent)
+    if stop_after and emails_sent >= stop_after:
+        print(u"Stopping early at {0} emails. See configuration.py stop_after".format(stop_after))
+        break
+    
 
 print(u"Sent {0} emails".format(emails_sent))
 for owner in email_log:
-	print (owner)
-
-new_name = u"{0}.{1}.xlsx".format(ns.filename.split(".xlsx")[0], datetime.datetime.now().strftime("%m%d%Y"))
-shutil.move(path2xlsx, os.path.join(BASE_DIR, "reports", new_name))
-
+    print (owner)
 
 if not ns.go:
-	raw_input("Press any key to exit.")
+    raw_input("Press any key to exit.")
